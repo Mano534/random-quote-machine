c3b4cd6cf30b3b6f1a6fd6fc49708db1cea4c53a
monmonmon175@yahoo.com <monmonmon175@yahoo.com>
2020-06-16 11:27:26 +0200

    Updates

---
+++ precache-manifest.41562e1f3bd017e15b8b20cba56d309f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f648360065a007396f6f0a19cfa10f",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "db4e8902d4f181baf0bf",
+    "url": "/random-quote-machine/static/css/2.ee7f7daa.chunk.css"
+  },
+  {
+    "revision": "ac8d67a4e42a9da1b207",
+    "url": "/random-quote-machine/static/css/main.a283b6fb.chunk.css"
+  },
+  {
+    "revision": "db4e8902d4f181baf0bf",
+    "url": "/random-quote-machine/static/js/2.62f7ccc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.62f7ccc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8d67a4e42a9da1b207",
+    "url": "/random-quote-machine/static/js/main.6347fae0.chunk.js"
+  },
+  {
+    "revision": "2cec414998c69e0a692f",
+    "url": "/random-quote-machine/static/js/runtime-main.5e7c1c70.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
